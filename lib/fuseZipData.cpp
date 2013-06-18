8494763cea101f77a6367a4d2e929d64508adfdc
Alexander Galanin <al@galanin.nnov.ru>
2013-06-18 10:59:26 +0400

    updated test for relative paths

---
+++ lib/fuseZipData.cpp
@@ -0,0 +1,106 @@
+////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) 2008-2013 by Alexander Galanin                          //
+//  al@galanin.nnov.ru                                                    //
+//  http://galanin.nnov.ru/~al                                            //
+//                                                                        //
+//  This program is free software; you can redistribute it and/or modify  //
+//  it under the terms of the GNU Lesser General Public License as        //
+//  published by the Free Software Foundation; either version 3 of the    //
+//  License, or (at your option) any later version.                       //
+//                                                                        //
+//  This program is distributed in the hope that it will be useful,       //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+//  GNU General Public License for more details.                          //
+//                                                                        //
+//  You should have received a copy of the GNU Lesser General Public      //
+//  License along with this program; if not, write to the                 //
+//  Free Software Foundation, Inc.,                                       //
+//  51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA               //
+////////////////////////////////////////////////////////////////////////////
+
+#include <zip.h>
+#include <syslog.h>
+#include <cerrno>
+#include <cassert>
+#include <stdexcept>
+
+#include "fuseZipData.h"
+
+FuseZipData::FuseZipData(const char *archiveName, struct zip *z, const char *cwd): m_zip(z), m_archiveName(archiveName), m_cwd(cwd)  {
+}
+
+FuseZipData::~FuseZipData() {
+    if (chdir(m_cwd.c_str()) != 0) {
+        syslog(LOG_ERR, "Unable to chdir() to archive directory %s. Trying to save file into /tmp",
+                m_cwd.c_str());
+        if (chdir(getenv("TMP")) != 0) {
+            chdir("/tmp");
+        }
+    }
+    int res = zip_close(m_zip);
+    if (res != 0) {
+        syslog(LOG_ERR, "Error while closing archive: %s", zip_strerror(m_zip));
+    }
+    for (filemap_t::iterator i = files.begin(); i != files.end(); ++i) {
+        delete i->second;
+    }
+}
+
+void FuseZipData::build_tree() {
+    FileNode *root_node = new FileNode(this, "", FileNode::ROOT_NODE_INDEX);
+    root_node->is_dir = true;
+
+    zip_int64_t n = zip_get_num_entries(m_zip, 0);
+    for (zip_int64_t i = 0; i < n; ++i) {
+        const char *name = zip_get_name(m_zip, i, ZIP_FL_ENC_RAW);
+        validateFileName(name);
+        try {
+            FileNode *node = new FileNode(this, name, i);
+            (void) node;
+        }
+        catch (const FileNode::AlreadyExists &e) {
+            // Only need to skip node creation
+        }
+    }
+}
+
+int FuseZipData::removeNode(FileNode *node) const {
+    node->detach();
+    zip_int64_t id = node->id;
+    delete node;
+    if (id >= 0) {
+        return (zip_delete (m_zip, id) == 0)? 0 : ENOENT;
+    } else {
+        return 0;
+    }
+}
+
+void FuseZipData::validateFileName(const char *fname) {
+    if (fname[0] == '/') {
+        throw std::runtime_error("absolute paths are not supported");
+    }
+    // anomalies
+    if (fname[0] == 0) {
+        throw std::runtime_error("empty file name");
+    }
+    if (strstr(fname, "//") != NULL) {
+        throw std::runtime_error(std::string("bad file name (two slashes): ") + fname);
+    }
+
+    // . or .. in file/dir name
+    assert(*fname != 0);
+    assert(*fname != '/');
+    const char *start = fname, *cur;
+    while ((cur = strchr(start + 1, '/')) != NULL) {
+        if ((cur - start == 1 && start[0] == '.') ||
+            (cur - start == 2 && start[0] == '.' && start[1] == '.')) {
+            throw std::runtime_error("paths relative to parent directory are not supported");
+        }
+        start = cur + 1;
+    }
+    // end of string is reached
+    if (strcmp(start, ".") == 0 || strcmp(start, "..") == 0) {
+        throw std::runtime_error("paths relative to parent directory are not supported");
+    }
+}
---
+++ lib/fuseZipData.h
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) 2008-2013 by Alexander Galanin                          //
+//  al@galanin.nnov.ru                                                    //
+//  http://galanin.nnov.ru/~al                                            //
+//                                                                        //
+//  This program is free software; you can redistribute it and/or modify  //
+//  it under the terms of the GNU Lesser General Public License as        //
+//  published by the Free Software Foundation; either version 3 of the    //
+//  License, or (at your option) any later version.                       //
+//                                                                        //
+//  This program is distributed in the hope that it will be useful,       //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+//  GNU General Public License for more details.                          //
+//                                                                        //
+//  You should have received a copy of the GNU Lesser General Public      //
+//  License along with this program; if not, write to the                 //
+//  Free Software Foundation, Inc.,                                       //
+//  51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA               //
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef FUSEZIP_DATA
+#define FUSEZIP_DATA
+
+#include <string>
+
+#include "types.h"
+#include "fileNode.h"
+
+class FuseZipData {
+private:
+    /**
+     * Check that file name does not contain paths relative to parent
+     * directory or absolute paths
+     * @throws std::runtime_exception if name is invalid
+     */
+    static void validateFileName(const char *fname);
+public:
+    filemap_t files;
+    struct zip *m_zip;
+    const char *m_archiveName;
+    std::string m_cwd;
+
+    /**
+     * Keep archiveName and cwd in class fields and build file tree from z.
+     *
+     * 'cwd' and 'z' free()-ed in destructor.
+     * 'archiveName' should be managed externally.
+     */
+    FuseZipData(const char *archiveName, struct zip *z, const char *cwd);
+    ~FuseZipData();
+
+    /**
+     * Detach node from tree, and delete associated entry in zip file if
+     * present.
+     *
+     * @param node Node to remove
+     * @return Error code or 0 is successful
+     */
+    int removeNode(FileNode *node) const;
+
+    /**
+     * Build tree of zip file entries from ZIP file
+     */
+    void build_tree();
+};
+
+#endif
+
